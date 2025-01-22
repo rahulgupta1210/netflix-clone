9fcb668d41b934541aa6c9afa939570068e2798b
rahulgupta1210 <pgupta.rahul88@gmail.com>
2025-01-22 19:29:04 +0530

    updated youtube data with auto play

--- Notes/notes2.md
+++ Notes/notes2.md
@@ -2,3 +2,12 @@
 # because of strict mode it ll call two times api,index.js only in local server in prod it ll be once
 # Register TMDB API and create and APP and get access token ,go to documentation
 # Go to TMDb now playing movies and get the api endpoint
+
+# created custom hookd for now playing movies
+# update store using movie slice
+- Planning for primary and secondary container
+-fetched trailer video
+-update store with trailer video data
+-embedded the youtube video and make it autoplay and mute
+-tailwidn classes for main container
+
--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -3,13 +3,16 @@ import { useSelector } from 'react-redux';
 import useMovieTrailer from '../hooks/useMovieTrailer';
 
 export const VideoBackground = ({ movieId }) => {
-  const trailervideo = useSelector((store) => store.movies?.trailervideo)
+  const trailervideo = useSelector((store) => store.movies.trailervideo);
+  //const trailervideo = useSelector((store) => store.movies?.trailervideo)
   useMovieTrailer(movieId);
-
+  console.log(trailervideo)
+  const id ='hR1-ihzff3I';
+  console.log("https://www.youtube.com/embed/" + id);
   return (
-    <div>
+    <div className='w-screen'>
       <iframe className='w-screen aspect-video'
-        src={"https://www.youtube.com/embed/" + trailervideo?.key}
+        src={"https://www.youtube.com/embed/" + id + "?&autoplay=1&mute=1"}
         title="YouTube video player" frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     </div>
